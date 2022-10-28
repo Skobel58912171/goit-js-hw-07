d3e723fd9ca615ff03060c1b9319443feb283358
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-10-28 14:45:07 +0300

    Add tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryModal = document.querySelector(".gallery");
+console.log(galleryModal);
+
+galleryModal.addEventListener("click", modalOpen);
+galleryModal.addEventListener("keydown", modalClose);
+let instance;
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryModal.insertAdjacentHTML("afterbegin", markup);
+
+function modalOpen(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  // console.log("click");
+  instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`);
+  instance.show();
+}
+
+function modalClose(evt) {
+  if (evt.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryModal = document.querySelector(".gallery");
+// console.log(galleryModal);
+
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+galleryModal.insertAdjacentHTML("afterbegin", markup);
+// console.log(galleryModal);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function (evt) {});
